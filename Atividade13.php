7764f1aa3311b693ac622828be99e7594e54904b
lucasvieirasenac <lucasvieirasenac@gmail.com>
2026-06-30 11:42:03 -0300

    alteração

--- Atividade13.php
+++ Atividade13.php
@@ -2,10 +2,11 @@
 
 $numero1 = 18;
 $numero2 = 20;
-$resultado = $numero1 + $numero2;
+$numero3 = 9;
+$resultado = $numero1 * $numero2 * $numero3;
 
-$sql = "INSERT INTO calculo (numero1, numero2, resultado)
-VALUES ($numero1, $numero2, $resultado)";
+$sql = "INSERT INTO calculo (numero1, numero2, numero3, resultado)
+VALUES ($numero1, $numero2, $numero3, $resultado)";
 
 if ($conexao->query($sql)) {
     echo "Dados Salvos com sucesso!";
