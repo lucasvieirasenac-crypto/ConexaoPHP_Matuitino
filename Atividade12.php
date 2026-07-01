5e680c02235cbc9761837676b18306ef9e077f75
lucasvieirasenac <lucasvieirasenac@gmail.com>
2026-07-01 09:30:14 -0300

    alterações

--- Atividade12.php
+++ Atividade12.php
@@ -1,16 +1,17 @@
 <?php 
+include "conexao.php";
 
 $numero1 = 18;
 $numero2 = 20;
 $resultado = $numero1 + $numero2;
 
-$sql = "INSERT INTO calculo (numero1, numero2, resultado)
+$sql = "INSERT INTO soma (numero1, numero2, resultado)
 VALUES ($numero1, $numero2, $resultado)";
 
 if ($conexao->query($sql)) {
-    echo "Dados Salvos com sucesso!";
+    echo "<br> Dados Salvos com sucesso!";
 } else {
-    echo "Erro ao salvar";
+    echo "<br> Erro ao salvar";
 }
 
 ?>
--- Atividade13.php
+++ Atividade13.php
@@ -10,9 +10,9 @@ $sql = "INSERT INTO calculo (numero1, numero2, numero3, resultado)
 VALUES ($numero1, $numero2, $numero3, $resultado)";
 
 if ($conexao->query($sql)) {
-    echo "Dados Salvos com sucesso!";
+    echo "<br> Dados Salvos com sucesso!";
 } else {
-    echo "Erro ao salvar";
+    echo "<br> Erro ao salvar";
 }
 
 
